918276fa54cdcc624200c3661b01cce922d6fc09
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-05-20 22:36:43 -0300

    Projeto final

--- HeroisMarvel/HeroisMarvel/HeroesTableViewController.swift
+++ HeroisMarvel/HeroisMarvel/HeroesTableViewController.swift
@@ -10,36 +10,71 @@ import UIKit
 
 class HeroesTableViewController: UITableViewController {
 
+    var name: String?
+    var heroes: [Hero] = []
     var label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
         label.textColor = .white
         return label
     }()
+    var loadingHeroes = false
+    var currentPage = 0
+    var total = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = "Buscando herois. Aguarde..."
+        loadHeroes()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! HeroViewController
+        vc.hero = heroes[tableView.indexPathForSelectedRow!.row]
+    }
+    
+    func loadHeroes() {
+        loadingHeroes = true
+        MarvelAPI.loadHeroes(name: name, page: currentPage) { (info) in
+            if let info = info {
+                self.heroes += info.data.results
+                self.total = info.data.total
+                DispatchQueue.main.async {
+                    self.loadingHeroes = false
+                    self.label.text = "Não foram encontrados herois com o nome \(self.name!)."
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        tableView.backgroundView = heroes.count == 0 ? label : nil
+        return heroes.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! HeroTableViewCell
+
+        let hero = heroes[indexPath.row]
+        cell.prepareCell(with: hero)
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == heroes.count - 10 && !loadingHeroes && heroes.count != total {
+            currentPage += 1
+            loadHeroes()
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
---
+++ HeroisMarvel/HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,51 @@
+//
+//  MarvelAPI.swift
+//  HeroisMarvel
+//
+//  Created by Henrique Alves Batochi on 20/05/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
+import SwiftHash
+import Alamofire
+
+class MarvelAPI {
+    
+    static private let basePath = "https://gateway.marvel.com/v1/public/characters?"
+    static private let privateKey = "f8be69c8ddf220330a472389d8039d575d3efc14"
+    static private let publicKey = "b19d7f07fb95b36df4754955083fa139"
+    static private let limit = 50
+    
+    class func loadHeroes(name: String?, page: Int = 0, onComplete: @escaping (MarvelInfo?) -> Void) {
+        let offset = page * limit
+        let startsWith: String
+        if let name = name, !name.isEmpty {
+            startsWith = "nameStartsWith=\(name.replacingOccurrences(of: " ", with: ""))&"
+        } else {
+            startsWith = ""
+        }
+        
+        let url = basePath + "offset=\(offset)&limit=\(limit)&" + startsWith + getCredencials()
+        Alamofire.request(url).responseJSON { (response) in
+            guard let data = response.data else {
+                onComplete(nil)
+                return
+            }
+            do {
+                let marvelInfo = try JSONDecoder().decode(MarvelInfo.self, from: data)
+                onComplete(marvelInfo)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private class func getCredencials() -> String {
+        let ts = String(Date().timeIntervalSince1970)
+        let hash = MD5(ts + privateKey + publicKey).lowercased()
+        return "ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+    }
+    
+    
+}
